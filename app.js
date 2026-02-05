24aa38dfe8cf6f74f45d54e8691025e1b034c4ea
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-02-05 17:56:01 +0800

    full project

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ const container = document.querySelector('.container');
 const btn = document.querySelector('.btn');
 
 const showPeople = () => {
-  const newPeople = people.map((person) => {
-    console.log(person);
-  });
+  const newPeople = people
+    .map((person) => {
+      const { name, job } = person;
+      return `<p>${name} <strong>${job}</strong></p>`;
+    })
+    .join('');
+  container.innerHTML = newPeople;
 };
 
 btn.addEventListener('click', () => {
