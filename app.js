79a584b2de56617d8c293f47f86976f7be3898ec
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-02-05 17:38:58 +0800

    full project

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+//MODULES ES6 - Simply put, Module is a one statement placed in different files
+
+const people = [
+  { name: 'john', job: 'developer' },
+  { name: 'susan', job: 'designer' },
+  { name: 'anna', job: 'the boss' },
+];
+
+console.log(people);
