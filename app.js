eaf015ef5090e0f7defd49f7194b91ac5e20c342
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-02-05 21:07:26 +0800

    full project

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 //MODULES ES6 - Simply put, Module is a one statement placed in different files
-import { random } from './utils/data.js';
-
-const people = [
-  { name: 'john', job: 'developer' },
-  { name: 'susan', job: 'designer' },
-  { name: 'anna', job: 'the boss' },
-];
+import { random, people } from './utils/data.js';
 
 // console.log(people);
 // console.log(random);
