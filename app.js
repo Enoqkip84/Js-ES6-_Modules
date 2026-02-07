daad5f68e947972e973dbc22269dd6fd02c4f34d
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-02-07 13:38:07 +0800

    full project

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 //MODULES ES6 - Simply put, Module is a one statement placed in different files
 import { random, people } from './utils/data.js';
-
+import showPeople from './utils/showPeople.js';
 // console.log(people);
 // console.log(random);
 
 const container = document.querySelector('.container');
 const btn = document.querySelector('.btn');
 
-const showPeople = () => {
-  const newPeople = people
-    .map((person) => {
-      const { name, job } = person;
-      return `<p>${name} <strong>${job}</strong></p>`;
-    })
-    .join('');
-  container.innerHTML = newPeople;
-};
+// container.innerHTML = newPeople;
 
 btn.addEventListener('click', () => {
-  showPeople();
+  container.innerHTML = showPeople(people);
 });
