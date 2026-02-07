59ce43fb96e809b9f8ef6f4e187dd6491604d353
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-02-07 13:42:15 +0800

    full project

--- utils/showPeople.js
+++ utils/showPeople.js
@@ -1,4 +1,28 @@
-const showPeople = (people) => {
+/*
+==================================================
+export default - first way
+==================================================
+*/
+
+// const showPeople = (people) => {
+//   const newPeople = people
+//     .map((person) => {
+//       const { name, job } = person;
+//       return `<p>${name} <strong>${job}</strong></p>`;
+//     })
+//     .join('');
+//   return newPeople;
+// };
+
+// export default showPeople;
+
+/*
+==================================================
+export default - second way
+==================================================
+*/
+
+export default (people) => {
   const newPeople = people
     .map((person) => {
       const { name, job } = person;
@@ -7,5 +31,3 @@ const showPeople = (people) => {
     .join('');
   return newPeople;
 };
-
-export default showPeople;
